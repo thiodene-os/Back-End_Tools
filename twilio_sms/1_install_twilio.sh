b0c1f4cac4a02f0d15fe2fe6058882c9b714f0cb
Serge Ayissi <ayissi.serge@gmail.com>
2019-03-01 14:23:09 -0500

    Create 1_install_twilio.sh

---
+++ twilio_sms/1_install_twilio.sh
@@ -0,0 +1,9 @@
+# Install the Twilio PHP Helper Library
+# We need to install the Twilio PHP Helper Library in the directory where you will complete the quickstart. 
+# If you are using Composer, there are two ways to do this.
+
+# First, from a terminal, you can run the following command:
+# -> Twilio needs Composer to be installed
+php composer.phar require twilio/sdk
+composer require twilio/sdk
+
