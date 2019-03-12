f802c9adada744a85709246b37ebf9ea0757f072
Serge Ayissi <ayissi.serge@gmail.com>
2019-03-12 10:35:31 -0400

    Create send_email_smtp2.php

---
+++ phpmailer_smtp/send_email_smtp2.php
@@ -0,0 +1,52 @@
+<?php
+
+//require 'PHPMailerAutoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+//require 'vendor/autoload.php';
+//require(__DIR__ . '/vendor/autoload.php');
+require('/root/vendor/autoload.php');
+
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'mail.bra******.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'ayissi_serge@thiodene.com';                 // SMTP username
+    $mail->Password = '********';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    //Recipients
+    //$mail->setFrom('alarm@scentroid.com', 'SIMS Alarm');
+    $mail->setFrom('info@forevent.com', 'Forevent Information');
+    $mail->addAddress('ayissi.serge@gmail.com', 'Serge Ayissi');     // Add a recipient
+    //$mail->addAddress('serge.a@scentroid.com', 'Serge Ayissi');     // Add a recipient
+    //$mail->addAddress('ardevan.b@scentroid.com', 'Ardevan Bakhtari');     // Add a recipient
+    //$mail->addAddress('ellen@example.com');               // Name is optional
+    $mail->addReplyTo('info@forevent.com', 'Forevent Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Sensor NO2-LC in SL101803';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}
+
+?>
