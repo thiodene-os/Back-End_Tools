084e3c4fee8a8d9b14a3e5715b3549df960221d0
Serge Ayissi <ayissi.serge@gmail.com>
2019-03-16 11:30:08 -0400

    Create 1_install_mailgun.sh

---
+++ mailgun_email/1_install_mailgun.sh
@@ -0,0 +1,3 @@
+# Use Composer and Autoload to install Mailgun libraries
+# On the command line with the right PHP version type the following command:
+php composer.phar require mailgun/mailgun-php php-http/guzzle6-adapter php-http/message
